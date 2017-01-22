a2d74e75d833a8d08f6a18120890352a45b90ef6
IKolyadko <IKolyadko@DESKTOP-TFG4K7Q>
2017-01-23 00:29:56 +0300

    add plane cs

---
+++ GraphicsModule.Geometry/Objects/Planes/Plane2D.cs
@@ -0,0 +1,10 @@
+﻿using GraphicsModule.Geometry.Objects.Points;
+
+namespace GraphicsModule.Geometry.Objects.Planes
+{
+    public class Plane2D
+    {
+        public Point2D[] Points { get; set; }
+
+    }
+}
---
+++ GraphicsModule.Geometry/Objects/Planes/Plane3D.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphicsModule.Geometry.Objects.Planes
+{
+    class Plane3D
+    {
+    }
+}
