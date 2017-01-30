53555bee6f4d0f0afdac479f39bbbef219404e5c
svkolyadko <svkolyadko@local>
2017-01-31 00:41:55 +0300

    Add PlaneMenuSelector

---
+++ GraphicsModule/Controls/Menu/PlaneMenuSelector.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Windows.Forms;
+using GraphicsModule.CreateObjects;
+
+namespace GraphicsModule.Controls.Menu
+{
+    public partial class PlaneMenuSelector : UserControl
+    {
+        private PictureBox _mainPictureBox;
+        private ToolStripButton _mainStripButton;
+        private StatusStrip _menuStrip;
+        public PlaneMenuSelector(PictureBox mainPictureBox, ToolStripButton mainStripButton, StatusStrip menuStrip)
+        {
+            InitializeComponent();
+            _mainPictureBox = mainPictureBox;
+            _mainStripButton = mainStripButton;
+            _menuStrip = menuStrip;
+        }
+        private void mainStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            Visible = false;
+            GraphicsControl.Operations = null;
+            _menuStrip.Visible = true;
+        }
+        private void buttonPlane2D_Click(object sender, EventArgs e)
+        {
+            _mainPictureBox.Cursor = System.Windows.Forms.Cursors.Cross;
+            GraphicsControl.SetObject = new CreateLine2D();
+            _mainStripButton.Image = buttonPlane2D.Image;
+        }
+        private void buttonPlaneOfPlane1X0Y_Click(object sender, EventArgs e)
+        {
+            _mainPictureBox.Cursor = System.Windows.Forms.Cursors.Cross;
+            GraphicsControl.SetObject = new CreateLineOfPlane1X0Y();
+            _mainStripButton.Image = buttonPlaneOfPlane1X0Y.Image;
+        }
+        private void buttonPlaneOfPlane2X0Z_Click(object sender, EventArgs e)
+        {
+            _mainPictureBox.Cursor = System.Windows.Forms.Cursors.Cross;
+            GraphicsControl.SetObject = new CreateLineOfPlane2X0Z();
+            _mainStripButton.Image = buttonPlaneOfPlane2X0Z.Image;
+        }
+        private void buttonPlaneOfPlane3Y0Z_Click(object sender, EventArgs e)
+        {
+            _mainPictureBox.Cursor = System.Windows.Forms.Cursors.Cross;
+            GraphicsControl.SetObject = new CreateLineOfPlane3Y0Z();
+            _mainStripButton.Image = buttonPlaneOfPlane3Y0Z.Image;
+        }
+        private void buttonPlane3D_Click(object sender, EventArgs e)
+        {
+            _mainPictureBox.Cursor = System.Windows.Forms.Cursors.Cross;
+            GraphicsControl.SetObject = new CreateLine3D();
+            _mainStripButton.Image = buttonPlane3D.Image;
+        }
+        private void buttonGeneratePlane3D_Click(object sender, EventArgs e)
+        {
+            _mainPictureBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            GraphicsControl.SetObject = new GenerateLine3D();
+            _mainStripButton.Image = buttonGeneratePlane3D.Image;
+        }
+    }
+}
